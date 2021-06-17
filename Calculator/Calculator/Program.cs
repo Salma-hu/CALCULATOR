1e6fc1d22a6e37e1410cd61e3a10208ee63df0a8
Salma Belassiri <salmabelassiri@gmail.com>
2021-06-18 00:15:28 +0100

    Add UnitTest

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Calculator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var clt = new unitCalcul () ;
+
+            clt.calcul();
         }
     }
 }
--- Calculator/TestCaclulator/UnitTest1.cs
+++ Calculator/TestCaclulator/UnitTest1.cs
@@ -1,13 +1,92 @@
 using System;
 using Xunit;
+using Calculator;
 
 namespace TestCaclulator
 {
     public class UnitTest1
     {
+
+        public unitCalcul clt = new unitCalcul ();
+
         [Fact]
-        public void Test1()
+        public void AddTest()
         {
+
+            // Arrange
+
+            
+
+            var s = clt.Addition(1,2);
+            
+            
+            //Assert
+
+              
+                Assert.Equal (3, s) ;
+
+
+        }
+
+
+
+        [Fact]
+        public void SubTest()
+        {
+
+            // Arrange
+
+
+
+            var s = clt.Subtraction(3, 2);
+
+
+            //Assert
+
+
+            Assert.Equal(2, s);
+
+
+        }
+
+
+        [Fact]
+        public void MultpTest()
+        {
+
+            // Arrange
+
+
+
+            var s = clt.Multiplication(3, 2);
+
+
+            //Assert
+
+
+            Assert.Equal(6, s);
+
+
+        }
+
+
+
+        [Fact]
+        public void DivTest()
+        {
+
+            // Arrange
+
+
+
+            var s = clt.Division(3, 2);
+
+
+            //Assert
+
+
+            Assert.Equal(1, s);
+
 
         }
     }
