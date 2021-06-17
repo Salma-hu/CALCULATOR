979a4d6072ec8d9ac745fc0cf1440ad9ab5dfe3b
Salma Belassiri <salmabelassiri@gmail.com>
2021-06-17 22:48:20 +0100

    create project

---
+++ Calculator/TestCaclulator/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestCaclulator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Calculator/calculatorTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using Calculator;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace calculatorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // ARRANGE
+
+            var nbr = new Program();
+
+            nbr.input_1
+        }
+    }
+}
